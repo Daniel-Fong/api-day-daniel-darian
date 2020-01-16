4f6f44d2ad15b9f17ab8ff8f26f8126cd6497eea
Daniel Fong <daniel.fong.mv@gmail.com>
2020-01-16 11:40:59 -0700

    exercise 5 or 6

---
+++ src/api.js
@@ -0,0 +1,23 @@
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/daniel-darian';
+
+function getItems() {    
+return fetch(`${BASE_URL}/items`)
+};
+
+function createItem(name) {
+    let newItem = JSON.stringify({
+        name: name,
+    });
+    return fetch(`${BASE_URL}/items`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: newItem
+    });
+};
+
+export default {
+    createItem,
+    getItems
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import $ from 'jquery';
-
+import api from './api';
 import 'normalize.css';
 import './index.css';
+import store from './store';
 
 import shoppingList from './shopping-list';
 
 const main = function () {
+  api.getItems()
+  .then(res => res.json())
+  .then((items) => {
+    items.forEach((item) => store.addItem(item));
+    shoppingList.render();
+  });
+
+  api.getItems()
+  .then(res => res.json())
+  .then(res => console.log(res));
+
+console.log(api.BASE_URL);
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
